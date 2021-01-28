f67afdc9aaf2db68b3313e0e24e70b36880a8719
aydingltp <aydngltp@gmail.com>
2021-01-28 23:28:58 +0300

    hatim-list veriler geliyor

--- OnlineHatim/ClientApp/src/components/HatimList.vue
+++ OnlineHatim/ClientApp/src/components/HatimList.vue
@@ -1,12 +1,39 @@
 ﻿<template>
 <div class="hatimlist">
 hatimlisttttt
+    <table>
+        <tr>
+            <th>Hatim İsmi</th>
+            <th>Bitiş Tarihi</th>
+            <th>Detaya git</th>
+        </tr>
+        <tr v-for="cuz in cuzler" :key="cuz.id">
+            <td>{{cuz.name}}</td>
+            <td>{{cuz.endDate}}</td>
+            <td></> {{cuz.urlCode}}</td>
+        </tr>
+    </table>
 </div>
 </template>
 
 <script>
-export default {
-name: 'HatimList',
+    import axios from "axios";
 
-}
+    export default {
+        name: 'HatimList',
+
+        data() {
+            return {
+                cuzler: [],
+                title: null
+
+            };
+        },
+        created() {
+            axios.get("api/hatim/", {}).then(obj => {
+                this.cuzler = obj.data;
+                console.log(obj.data);
+            });
+        }
+    }
 </script>
