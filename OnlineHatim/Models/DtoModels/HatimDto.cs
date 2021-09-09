dbaf6ebd1d1fd2d1ab3768e2e50fd8ee706d2bb8
aydingltp <aydngltp@gmail.com>
2021-09-09 10:04:11 +0300

    fix

--- OnlineHatim/Models/DtoModels/HatimDto.cs
+++ OnlineHatim/Models/DtoModels/HatimDto.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OnlineHatim.Models.DtoModels
 {
@@ -12,9 +10,5 @@ namespace OnlineHatim.Models.DtoModels
         public string UrlCode { get; set; }
         public List<HatimCuz> HatimCuz { get; set; }
         public bool IsEnd { get; set; } = false;
-
-
-
-
     }
 }
